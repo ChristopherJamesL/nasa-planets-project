52dd05420f0d1d6bbbd528530fdfb32e274f32e2
ChristopherL <anbchris@gmail.com>
2025-06-04 19:48:05 -0400

    writing more server tests for launches endpoint, converting jest over to nodes built in testing library

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,3 +1,6 @@
+const { describe, test } = require('node:test');
+const { deepStrictEqual } = require('node:assert');
+
 const request = require('supertest');
 const app = require('../../app');
 
@@ -24,6 +27,27 @@ describe('Test POST /launches', () => {
         target: 'Kepler-186 f',
     };
 
+    const launchDataWithInvalidDate = {
+        launchDate: 'zoot',
+        mission: 'USS Enterprise',
+        rocket: 'NCC 170-D',
+        target: 'Kepler-186 f',
+    };
+
+    const launchWithInvalidHistoricalDate = {
+        launchDate: 'January 4, 1950',
+        mission: 'USS Enterprise',
+        rocket: 'NCC 170-D',
+        target: 'Kepler-186 f',
+    };
+
+    const launchWithInvalidCurrentDate = {
+        launchDate: new Date().toISOString(),
+        mission: 'USS Enterprise',
+        rocket: 'NCC 170-D',
+        target: 'Kepler-186 f',
+    };
+
     test('It should respond with 201 created', async () => {
         const response = await request(app)
             .post('/launches')
@@ -33,16 +57,57 @@ describe('Test POST /launches', () => {
 
         const requestDate = new Date(completeLaunchData.launchDate).valueOf();
         const responseDate = new Date(response.body.launchDate).valueOf();
-        expect(requestDate).toBe(responseDate);
-            
-        expect(response.body).toMatchObject(launchDataWithoutDate);
+        deepStrictEqual(requestDate, responseDate);
+           
+        const { mission, rocket, target } = response.body;
+        deepStrictEqual({ mission, rocket, target }, launchDataWithoutDate);
     });
 
-    test('It should catch missing required properties', () => {
+    test('It should catch missing required properties', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(launchDataWithoutDate)
+            .expect('Content-Type', /json/)
+            .expect(400);
 
+        deepStrictEqual(response.body, {
+            error: `Missing required launch property`,
+        });
     });
 
-    test('It should catch invalid dates', () => {
+    test('It should catch invalid dates', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(launchDataWithInvalidDate)
+            .expect('Content-Type', /json/)
+            .expect(400);
 
+        deepStrictEqual(response.body, {
+            error: 'Invalid launch date',
+        });
+    });
+
+    test('It should catch a launch date set in history', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(launchWithInvalidHistoricalDate)
+            .expect('Content-Type', /json/)
+            .expect(400);
+
+        deepStrictEqual(response.body, {
+            error: 'Launch date must be in the future',
+        });
+    });
+
+    test('It should catch a launch date set to the current day', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(launchWithInvalidCurrentDate)
+            .expect('Content-Type', /json/)
+            .expect(400);
+
+        deepStrictEqual(response.body, {
+            error: 'Launch date must be in the future',
+        })
     })
 });
