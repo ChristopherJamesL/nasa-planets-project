25be998cb2727f9043631628cfd8a162fe79e254
ChristopherL <anbchris@gmail.com>
2025-06-09 23:27:36 -0400

    beginning to implement db

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = require('./app');
 
@@ -6,9 +8,25 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URI = process.env.MONGO_URI;
+
 const server = http.createServer(app);
 
+mongoose.connection.on('open', () => {
+    console.log(`MongoDB connection ready!`);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('error: ', err);
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    });
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(`listening on port ${PORT}...`);
