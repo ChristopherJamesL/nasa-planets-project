9e41bb5981fa5fa5f6238e7222063db0d56a7b24
ChristopherL <anbchris@gmail.com>
2025-06-10 20:19:46 -0400

    insert planet names into database.

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById } = require('../../models/launches.model');
+const { 
+    getAllLaunches, 
+    addNewLaunch, 
+    existsLaunchWithId, 
+    abortLaunchById 
+} = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
